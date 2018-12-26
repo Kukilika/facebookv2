73ddd9c66a96fc4548ba869924b711de0210a187
Daniel Simion <37666663+DanielOctavian97@users.noreply.github.com>
2018-12-26 18:03:18 +0200

    Created New MCV Project with Authentication

---
+++ FacebookV2/FacebookV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FacebookV2.MvcApplication" Language="C#" %>
---
+++ FacebookV2/FacebookV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FacebookV2.Startup))]
+namespace FacebookV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
